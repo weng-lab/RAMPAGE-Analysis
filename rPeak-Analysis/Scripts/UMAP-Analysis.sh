716ad04da35106824c68dbb9762e64bdf0aea933
Jill Moore <Jill.Moore@umassmed.edu>
2021-01-06 12:13:07 -0500

    added umap

---
+++ rPeak-Analysis/Scripts/UMAP-Analysis.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Jill E Moore
+#Weng Lab
+#UMass Medical School
+#January 2021
+
+
+data=../RAMPAGE-List-Annotated.txt
+signalDir=~/Lab/ENCODE/RAMPAGE/signal-output
+scriptDir=~/GitHub/RAMPAGE-Analysis/rPeak-Analysis/Scripts
+
+awk -F "\t" '{if ($3 == "tissue") print $0}' $data > tmp.list
+q=$(wc -l tmp.list | awk '{print $1}')
+exp=$(awk -F "\t" '{if (NR == 1) print $1}' tmp.list)
+bio=$(awk -F "\t" '{if (NR == 1) print $2}' tmp.list)
+awk 'BEGIN{print "rPeak" "\t" "'$bio'"}{print $1 "\t" $2}' $signalDir/$exp.txt > tmp.matrix
+for j in `seq 2 1 $q`
+do
+    echo $j
+    exp=$(awk -F "\t" '{if (NR == '$j') print $1}' tmp.list)
+    bio=$(awk -F "\t" '{if (NR == '$j') print $2}' tmp.list)
+    awk 'BEGIN{print "'$bio'"}{print $2}' $signalDir/$exp.txt > tmp.col
+    paste tmp.matrix tmp.col > tmp.tmp
+    mv tmp.tmp tmp.matrix
+done
+mv tmp.matrix Tissue-RPKM-Matrix.txt
+
+cp $data tmp.list
+q=$(wc -l tmp.list | awk '{print $1}')
+exp=$(awk -F "\t" '{if (NR == 1) print $1}' tmp.list)
+bio=$(awk -F "\t" '{if (NR == 1) print $2}' tmp.list)
+awk 'BEGIN{print "rPeak" "\t" "'$bio'"}{print $1 "\t" $2}' $signalDir/$exp.txt > tmp.matrix
+for j in `seq 2 1 $q`
+do
+    echo $j
+    exp=$(awk -F "\t" '{if (NR == '$j') print $1}' tmp.list)
+    bio=$(awk -F "\t" '{if (NR == '$j') print $2}' tmp.list)
+    awk 'BEGIN{print "'$bio'"}{print $2}' $signalDir/$exp.txt > tmp.col
+    paste tmp.matrix tmp.col > tmp.tmp
+    mv tmp.tmp tmp.matrix
+done
+mv tmp.matrix All-Biosample-RPKM-Matrix.txt
+
+python $scriptDir/run-umap.py Tissue-RPKM-Matrix.txt 10 > UMAP-Tissue-10.txt
+python $scriptDir/run-umap.py All-Biosample-RPKM-Matrix.txt 10 > UMAP-All-10.txt
---
+++ rPeak-Analysis/Scripts/run-umap.py
@@ -0,0 +1,36 @@
+#Jill E Moore
+#Weng Lab
+#UMass Medical School
+#January 2021
+
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import sys, math
+import umap
+
+def Process_Matrix(matrixFile):
+    dataMatrix=[]
+    header=next(matrixFile).rstrip().split("\t")[1:]
+    for line in matrixFile:
+        line=[float(i) for i in line.rstrip().split("\t")[1:]]
+        line=[math.log(i+0.01,10) for i in line]
+        dataMatrix.append(line)
+    dataMatrix= np.array(dataMatrix)
+    dataMatrix=StandardScaler().fit_transform(dataMatrix)
+    dataMatrix=dataMatrix.transpose()
+    return header, dataMatrix
+        
+
+matrixFile=open(sys.argv[1])
+header, dataMatrix = Process_Matrix(matrixFile)
+
+reducer = umap.UMAP(n_neighbors = int(sys.argv[2]))
+
+x = reducer.fit_transform(dataMatrix)
+
+
+i=0
+for entry in x:
+    print(header[i], "\t", entry[0], "\t", entry[1])
+    i+=1
+
