096eff175215752733fe0c49d3dcc5c134d24b8f
Jill Moore <Jill.Moore@umassmed.edu>
2021-03-31 09:36:57 -0400

    added boundary variation scripts

---
+++ rPeak-Analysis/Scripts/Compare-Boundary-Variation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Jill E Moore
+#Weng Lab
+#UMass Medical School
+#March 2021
+
+scriptDir=~/GitHub/RAMPAGE-Analysis/rPeak-Analysis/Scripts
+dataDir=~/Lab/ENCODE/RAMPAGE/Peaks
+rPeakSummary=$dataDir/rPeaks-Filtered-Summary.txt
+allPeaks=$dataDir/All-Peaks.bed
+genomicContext=~/Lab/ENCODE/RAMPAGE/Genomic-Context/Full/All-RAMPAGE.genomic-context
+
+cd ~/Lab/ENCODE/RAMPAGE/Boundary-Variation
+
+bedtools intersect -wo -s -a $rPeakSummary -b $allPeaks > tmp.intersection
+python ~/Projects/RAMPAGE/calculate-boundary-variation.py tmp.intersection | \
+    sort -k1,1 > tmp.annotated
+sort -k4,4 $genomicContext | awk '{print $4 "\t" $(NF-1)}' | \
+    awk 'FNR==NR {x[$1];next} ($1 in x)' tmp.annotated - | \
+    paste tmp.annotated - > Boundary-Variation-Summary.txt
+
+rm tmp.*
---
+++ rPeak-Analysis/Scripts/calculate-boundary-variation.py
@@ -0,0 +1,38 @@
+#Jill E Moore
+#Weng Lab
+#UMass Medical School
+#March 2021
+
+import sys, numpy
+
+peakDict={}
+
+intersections=open(sys.argv[1])
+for line in intersections:
+    line=line.rstrip().split("\t")
+    if line[10] not in peakDict:
+        peakDict[line[10]]=[[],[],[], int(line[2])-int(line[1])]
+    if line[3] != line[14]:
+        summit1=int(line[3].split("_")[2])
+        summit2=int(line[14].split("_")[2])
+        peakDict[line[10]][0].append(abs(summit1-summit2))
+        
+        start1=int(line[1])
+        start2=int(line[12])
+        end1=int(line[2])
+        end2=int(line[13])
+        peakDict[line[10]][2].append(numpy.median([abs(start1-start2),abs(end1-end2)]))
+        
+        start701=int(line[6])
+        start702=int(line[17])
+        end701=int(line[7])
+        end702=int(line[18])
+        peakDict[line[10]][1].append(numpy.median([abs(start701-start702),abs(end701-end702)]))
+        
+intersections.close()
+
+for key in peakDict:
+    if len(peakDict[key][0]) >= 1:
+        print key, "\t", numpy.median(peakDict[key][0]), "\t", \
+            numpy.median(peakDict[key][1]), "\t", numpy.median(peakDict[key][2]), "\t", len(peakDict[key][2]), \
+            "\t", peakDict[key][3]
