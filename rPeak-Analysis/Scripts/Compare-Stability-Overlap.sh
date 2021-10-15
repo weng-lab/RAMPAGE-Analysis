0bf681068d4a6f4549a8c853d6bfeb86db078e4f
Jill Moore <Jill.Moore@umassmed.edu>
2021-10-15 08:09:50 -0400

    added stability comparison

---
+++ rPeak-Analysis/Scripts/Compare-Stability-Overlap.sh
@@ -0,0 +1,29 @@
+biosample=$1
+header=$2
+bioLower=$(echo $biosample | awk '{print tolower($0)}')
+
+~/bin/liftOver ~/Lab/ENCODE/RAMPAGE/$biosample-Comparison/RAMPAGE-$biosample-RPM-2.peaks.bed ~/Lab/Reference/Human/hg38/hg38ToHg19.over.chain  tmp.hg19 tmp.no
+
+totalP=$(wc -l "tss_SS_"$bioLower"_plus.bed" | awk '{print $1}')
+totalN=$(wc -l "tss_SS_"$bioLower"_minus.bed" | awk '{print $1}')
+
+bedtools intersect -u -s -a "tss_SS_"$bioLower"_plus.bed" -b tmp.hg19 | wc -l | awk '{print $1 "\t" '$totalP' "\t" $1/'$totalP'}'
+bedtools intersect -u -s -a "tss_SS_"$bioLower"_minus.bed" -b tmp.hg19 | wc -l | awk '{print $1 "\t" '$totalN' "\t" $1/'$totalN'}'
+
+totalP=$(wc -l "tss_US_"$bioLower"_plus.bed" | awk '{print $1}')
+totalN=$(wc -l "tss_US_"$bioLower"_minus.bed" | awk '{print $1}')
+
+bedtools intersect -u -s -a "tss_US_"$bioLower"_plus.bed" -b tmp.hg19 | wc -l | awk '{print $1 "\t" '$totalP' "\t" $1/'$totalP'}'
+bedtools intersect -u -s -a "tss_US_"$bioLower"_minus.bed" -b tmp.hg19 | wc -l | awk '{print $1 "\t" '$totalN' "\t" $1/'$totalN'}'
+
+totalP=$(wc -l "tss_SU_"$bioLower"_plus.bed" | awk '{print $1}')
+totalN=$(wc -l "tss_SU_"$bioLower"_minus.bed" | awk '{print $1}')
+
+bedtools intersect -u -s -a "tss_SU_"$bioLower"_plus.bed" -b tmp.hg19 | wc -l | awk '{print $1 "\t" '$totalP' "\t" $1/'$totalP'}'
+bedtools intersect -u -s -a "tss_SU_"$bioLower"_minus.bed" -b tmp.hg19 | wc -l | awk '{print $1 "\t" '$totalN' "\t" $1/'$totalN'}'
+
+totalP=$(wc -l "tss_UU_"$bioLower"_plus.bed" | awk '{print $1}')
+totalN=$(wc -l "tss_UU_"$bioLower"_minus.bed" | awk '{print $1}')
+
+bedtools intersect -u -s -a "tss_UU_"$bioLower"_plus.bed" -b tmp.hg19 | wc -l | awk '{print $1 "\t" '$totalP' "\t" $1/'$totalP'}'
+bedtools intersect -u -s -a "tss_UU_"$bioLower"_minus.bed" -b tmp.hg19 | wc -l | awk '{print $1 "\t" '$totalN' "\t" $1/'$totalN'}'
