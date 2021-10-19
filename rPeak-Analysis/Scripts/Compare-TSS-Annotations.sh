803bb4a082a64b3b4bf714d6380194fadca18b7c
Jill Moore <Jill.Moore@umassmed.edu>
2021-10-19 13:17:44 -0400

    added gro peak intersection

--- rPeak-Analysis/Scripts/Compare-TSS-Annotations.sh
+++ rPeak-Analysis/Scripts/Compare-TSS-Annotations.sh
@@ -3,12 +3,13 @@
 #Jill E Moore
 #Weng Lab
 #UMass Medical School
-#January 2021
+#October 2021
 
 biosample=$1
 
 rampagePeaks=~/Lab/ENCODE/RAMPAGE/$biosample-Comparison/RAMPAGE-$biosample-RPM-2.peaks.bed
 cagePeaks=~/Lab/ENCODE/RAMPAGE/$biosample-Comparison/CAGE-$biosample-TPM-2.peaks.bed
+groPeaks=~/Lab/ENCODE/RAMPAGE/GRO-PRO/"TSS_All_"$biosample.hg38.bed
 groCapDir=~/Lab/ENCODE/RAMPAGE/GROcap
 longReadDir=~/Lab/ENCODE/RAMPAGE/PacBio
 
@@ -28,26 +29,26 @@ totalPacBio=$(cat $longReadDir/$biosample.5ends.minus.bed $longReadDir/$biosampl
 
 #RAMPAGE
 rampageCAGE=$(bedtools intersect -u -s -F 0.25 -a $rampagePeaks -b $cagePeaks | wc -l | \
-    awk '{print $1/'$totalRAMPAGE'*100}')
+    awk '{print $1/'$totalRAMPAGE'*100","$1}')
 awk '{if ($6 == "+") print $0}' $rampagePeaks > plus.bed
 awk '{if ($6 == "-") print $0}' $rampagePeaks > minus.bed
 bedtools intersect -u -a plus.bed -b $longReadDir/$biosample.5ends.plus.bed > pacbio
 bedtools intersect -u -a minus.bed -b $longReadDir/$biosample.5ends.minus.bed >> pacbio
-rampagePACBIO=$(wc -l pacbio | awk '{print $1/'$totalRAMPAGE'*100}')
+rampagePACBIO=$(wc -l pacbio | awk '{print $1/'$totalRAMPAGE'*100","$1}')
 rampageGRO=$(awk '{if ($2 > '$cutoff' || $2 < -1*'$cutoff') print $0}' $groCapDir/$biosample-GROcap-RAMPAGE.txt | \
-    wc -l | awk '{print $1/'$totalRAMPAGE'*100}')
+    wc -l | awk '{print $1/'$totalRAMPAGE'*100","$1}')
 echo -e "RAMPAGE \t --- \t" $rampageCAGE "\t" $rampagePACBIO "\t" $rampageGRO
 
 #CAGE
 cageRAMPAGE=$(bedtools intersect -u -s -f 0.25 -a $cagePeaks -b $rampagePeaks | wc -l | \
-    awk '{print $1/'$totalCAGE'*100}')
+    awk '{print $1/'$totalCAGE'*100","$1}')
 awk '{if ($6 == "+") print $0}' $cagePeaks > plus.bed
 awk '{if ($6 == "-") print $0}' $cagePeaks > minus.bed
 bedtools intersect -u -a plus.bed -b $longReadDir/$biosample.5ends.plus.bed > pacbio
 bedtools intersect -u -a minus.bed -b $longReadDir/$biosample.5ends.minus.bed >> pacbio
-cagePACBIO=$(wc -l pacbio | awk '{print $1/'$totalCAGE'*100}')
+cagePACBIO=$(wc -l pacbio | awk '{print $1/'$totalCAGE'*100","$1}')
 cageGRO=$(awk '{if ($2 > '$cutoff' || $2 < -1*'$cutoff') print $0}' $groCapDir/$biosample-GROcap-CAGE.txt | \
-    wc -l | awk '{print $1/'$totalCAGE'*100}')
+    wc -l | awk '{print $1/'$totalCAGE'*100","$1}')
 echo -e "CAGE \t" $cageRAMPAGE "\t" "---" "\t" $cagePACBIO "\t" $cageGRO
 
 #PacBio
@@ -55,14 +56,30 @@ awk '{if ($6 == "+") print $0}' $rampagePeaks > plus.bed
 awk '{if ($6 == "-") print $0}' $rampagePeaks > minus.bed
 bedtools intersect -u -b plus.bed -a $longReadDir/$biosample.5ends.plus.bed > pacbio
 bedtools intersect -u -b minus.bed -a $longReadDir/$biosample.5ends.minus.bed >> pacbio
-pacbioRAMPAGE=$(wc -l pacbio | awk '{print $1/'$totalPacBio'*100}')
+pacbioRAMPAGE=$(wc -l pacbio | awk '{print $1/'$totalPacBio'*100","$1}')
 awk '{if ($6 == "+") print $0}' $cagePeaks > plus.bed
 awk '{if ($6 == "-") print $0}' $cagePeaks > minus.bed
 bedtools intersect -u -b plus.bed -a $longReadDir/$biosample.5ends.plus.bed > pacbio
 bedtools intersect -u -b minus.bed -a $longReadDir/$biosample.5ends.minus.bed >> pacbio
-pacbioCAGE=$(wc -l pacbio | awk '{print $1/'$totalPacBio'*100}')
+pacbioCAGE=$(wc -l pacbio | awk '{print $1/'$totalPacBio'*100","$1}')
 pacbioGRO=$(awk '{if ($2 > '$cutoff' || $2 < -1*'$cutoff') print $0}' $groCapDir/$biosample-GROcap-PacBio.txt | \
-    wc -l | awk '{print $1/'$totalPacBio'*100}')
+    wc -l | awk '{print $1/'$totalPacBio'*100","$1}')
 echo -e "PacBio \t" $pacbioRAMPAGE "\t" $pacbioCAGE "\t" "---" "\t" $pacbioGRO
+
+#GroPeak
+totalGRO=$(wc -l $groPeaks | awk '{print $1}')
+groRAMPAGE=$(bedtools intersect -u -s -a $groPeaks -b $rampagePeaks | wc -l | \
+    awk '{print $1/'$totalGRO'*100","$1}')
+groCAGE=$(bedtools intersect -u -s -a $groPeaks -b $cagePeaks | wc -l | \
+    awk '{print $1/'$totalGRO'*100","$1}')
+
+awk '{if ($6 == "+") print $0}' $groPeaks > plus.bed 
+awk '{if ($6 == "-") print $0}' $groPeaks > minus.bed
+
+bedtools intersect -u -a plus.bed -b $longReadDir/$biosample.5ends.plus.bed > pacbio
+bedtools intersect -u -a minus.bed -b $longReadDir/$biosample.5ends.minus.bed >> pacbio
+groPACBIO=$(wc -l pacbio | awk '{print $1/'$totalGRO'*100","$1}')
+
+echo -e "GROcap \t" $groRAMPAGE "\t" $groCAGE "\t" $groPACBIO  "\t" "---"
 
 rm plus.bed minus.bed pacbio
